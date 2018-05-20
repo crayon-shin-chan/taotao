ed6b15c6a5d8418ec21aae43b92b1482e0c0f669
czy12345 <1253665625@qq.com>
2018-05-20 14:08:12 +0800

    DataSource ok

--- taotao-shop/src/main/java/czy/taotao/shop/config/RootConfig.java
+++ taotao-shop/src/main/java/czy/taotao/shop/config/RootConfig.java
@@ -1,8 +1,26 @@
 package czy.taotao.shop.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.apache.commons.dbcp2.BasicDataSource;
+import javax.sql.DataSource;
+import java.sql.DriverManager;
+import java.sql.Driver;
+import java.lang.System;
 
 @Configuration
 public class RootConfig {
+
+	@Bean
+	DataSource dataSource() {
+		System.out.println("创建了数据源");
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("dbc:mysql://localhost:3306/test");
+		ds.setUsername("root");
+		ds.setPassword("123456");
+		return ds;
+	}
 
 }
