72167bd9ff0eb7415deb6b690a2a4569c47b311b
czy12345 <1253665625@qq.com>
2018-05-20 14:44:57 +0800

    SqlSessionFactoryBean fail

--- taotao-shop/src/main/java/czy/taotao/shop/config/RootConfig.java
+++ taotao-shop/src/main/java/czy/taotao/shop/config/RootConfig.java
@@ -1,12 +1,12 @@
 package czy.taotao.shop.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.apache.commons.dbcp2.BasicDataSource;
 import javax.sql.DataSource;
-import java.sql.DriverManager;
-import java.sql.Driver;
 import java.lang.System;
 
 @Configuration
@@ -14,13 +14,22 @@ public class RootConfig {
 
 	@Bean
 	DataSource dataSource() {
-		System.out.println("创建了数据源");
 		BasicDataSource ds = new BasicDataSource();
 		ds.setDriverClassName("com.mysql.jdbc.Driver");
 		ds.setUrl("dbc:mysql://localhost:3306/test");
 		ds.setUsername("root");
 		ds.setPassword("123456");
+		ds.setInitialSize(10);
 		return ds;
 	}
 
+	@Bean
+	SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource ){
+		System.out.println(((BasicDataSource)dataSource).getDriverClassName());
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		//bean.setConfigLocation(new ClassPathResource("mybatis.xml"));
+		return bean;
+	}
+	
 }
