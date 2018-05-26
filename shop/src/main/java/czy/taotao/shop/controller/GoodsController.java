6a889f3a99d6216af48f114a41d3418fedc13dcb
czy12345 <1253665625@qq.com>
2018-05-26 13:28:27 +0800

    rename

--- shop/src/main/java/czy/taotao/shop/controller/GoodsController.java
+++ shop/src/main/java/czy/taotao/shop/controller/GoodsController.java
@@ -12,11 +12,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class GoodsController {
 
 	@RequestMapping(value = "/goods", method = RequestMethod.GET)
-	public List<Integer> goods() {
-		List<Integer> list = new ArrayList<Integer>(3);
-		list.add(1);
-		list.add(2);
-		list.add(3);
+	public List<User> goods() {
+		List<User> list = new ArrayList(3);
+		User user1 = new User();
+		user1.setName("张三");
+		user1.setAge(23);
+		User user2 = new User();
+		user2.setName("李四");
+		user2.setAge(56);
+		list.add(user1);
+		list.add(user2);
+		list.add(null);
 		return list;
 	}
 
