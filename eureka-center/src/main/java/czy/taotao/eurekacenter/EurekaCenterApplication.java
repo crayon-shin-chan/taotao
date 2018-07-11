16a22fd37129e2aaf0c4b561e43914fe46cc0af2
czy12345 <1253665625@qq.com>
2018-07-11 23:28:34 +0800

    完成服务中心配置

--- eureka-center/src/main/java/czy/taotao/eurekacenter/EurekaCenterApplication.java
+++ eureka-center/src/main/java/czy/taotao/eurekacenter/EurekaCenterApplication.java
@@ -2,8 +2,10 @@ package czy.taotao.eurekacenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaCenterApplication {
 
 	public static void main(String[] args) {
