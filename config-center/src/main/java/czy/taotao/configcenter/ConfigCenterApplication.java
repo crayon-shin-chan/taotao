3d6ad0c2a3dfff03b70a0b5d98c9a7c1e319cfe0
陈振远 <1253665625@qq.com>
2018-08-03 10:31:59 +0800

    添加注释

--- config-center/src/main/java/czy/taotao/configcenter/ConfigCenterApplication.java
+++ config-center/src/main/java/czy/taotao/configcenter/ConfigCenterApplication.java
@@ -9,6 +9,18 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigCenterApplication {
 
+	/**
+	 * 配置中心访问配置文件路由占位符：
+	 * name：应用程序名称
+	 * profile：当前激活的profile
+	 * label：当前的标签，在版本控制系统中有明确意义
+	 *
+	 * 配置中心访问指定配置文件有以下方法：
+	 * /{name}-{profile}.(yml|json|properties)：这种方式可以直接获取配置文件内容，而没有其他嵌套的字段
+	 * /{label}/name}-{profile}.(yml|json|properties)：这种方式获取结果与上面一样，但是可以加上标签
+	 * /{name}/{profile}：这种方式获取到的配置文件会嵌套在返回jaon结果中，作为一个字段返回
+	 * /{name}/{profile}/{label}：可以获取指定标签内容
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigCenterApplication.class, args);
 	}
