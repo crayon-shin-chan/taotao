6024aa8b8950732f4d59fd00966a355f56ed8eaa
陈振远 <1253665625@qq.com>
2018-08-03 14:29:27 +0800

    配置中心、服务中心暂时ok

--- eureka-center/src/main/java/czy/taotao/eurekacenter/EurekaCenterApplication.java
+++ eureka-center/src/main/java/czy/taotao/eurekacenter/EurekaCenterApplication.java
@@ -4,14 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+/**
+ * 启用服务注册中心
+ */
 @EnableEurekaServer
+@SpringBootApplication
 public class EurekaCenterApplication {
 
 	/**
 	 * 服务中心管理面板包含两个端点：
 	 * /：主管理面板
 	 * /lastn：也是一个面板，不过内容不同
+	 *
+	 * 注册端点为：
+	 * /eureka
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaCenterApplication.class, args);
