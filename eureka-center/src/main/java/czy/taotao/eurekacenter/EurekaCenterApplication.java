da53f07092993e944816c44038f8b4f6df877834
陈振远 <1253665625@qq.com>
2018-08-03 13:03:14 +0800

    配置中心、服务中心互相注册成功

--- eureka-center/src/main/java/czy/taotao/eurekacenter/EurekaCenterApplication.java
+++ eureka-center/src/main/java/czy/taotao/eurekacenter/EurekaCenterApplication.java
@@ -8,6 +8,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaCenterApplication {
 
+	/**
+	 * 服务中心管理面板包含两个端点：
+	 * /：主管理面板
+	 * /lastn：也是一个面板，不过内容不同
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaCenterApplication.class, args);
 	}
