48e17fbe2747f80e6603db1cae9f4606e00c964d
陈振远 <1253665625@qq.com>
2018-08-08 18:35:58 +0800

    添加一个pre类型的Zuul过滤器

---
+++ zuul-service/src/main/java/czy/taotao/zuul/filter/PreFilter.java
@@ -0,0 +1,69 @@
+package czy.taotao.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.ZuulFilterResult;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
+/**
+ * 配置一个pre类型的Zuul过滤器，用于在请求被路由之前拦截请求
+ */
+public class PreFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(PreFilter.class);
+    private static final Random random = new Random();
+
+    @Override
+    public boolean isStaticFilter() {
+        return false;
+    }
+
+    @Override
+    public boolean isFilterDisabled() {
+        return false;
+    }
+
+    @Override
+    public ZuulFilterResult runFilter() {
+        return super.runFilter();
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        logger.info("接收到一个请求："+request.getRequestURI());
+        int r = random.nextInt(10);
+        if(r%2==1){
+            logger.info("随机出一个奇数，请求不会被路由");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(500);
+            context.setResponseBody("因为运气不佳，请求被拦截");
+        }else{
+            logger.info("随机出一个偶数，请求将会被路由");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(200);
+        }
+        return null;
+    }
+}
