e5c8952896d310dd6002253074309d0f5f415792
陈振远 <1253665625@qq.com>
2018-08-08 10:02:42 +0800

    熔断器监控成功

--- consumer-service/src/main/java/czy/taotao/consumer/ConsumerApplication.java
+++ consumer-service/src/main/java/czy/taotao/consumer/ConsumerApplication.java
@@ -2,14 +2,18 @@ package czy.taotao.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  *  必须@EnableFeignClients，才可以扫描到FeignClient注解的Bean
+ *  必须@EnableHystrixDashboard，才能使用仪表盘监控熔断器情况
  */
 @EnableFeignClients
 @SpringBootApplication
+@EnableHystrixDashboard
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
---
+++ consumer-service/src/main/java/czy/taotao/consumer/config/HystrixDashboardConfiguration.java
@@ -0,0 +1,29 @@
+package czy.taotao.consumer.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class HystrixDashboardConfiguration {
+
+    /**
+     * 注册返回/hystrix.stream流数据的Servlet
+     * 管理页面通过polling轮训获取最新的熔断器监控情况
+     * 注意：一开始未访问服务时，管理页面显示loading字样
+     * 直到第一个服务消费请求出现，管理页面才出现监控面板
+     *
+     * 默认的配置有一个proxy.stream的流，但是好像不起作用
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
