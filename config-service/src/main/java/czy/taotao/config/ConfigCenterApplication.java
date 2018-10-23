b524fb8f3601b89a1a0d15b957d4946726a6e39e
chenzhenyuan@skyable.cn <1253665625@qq.com>
2018-10-23 09:36:22 +0800

    添加路由说明

--- config-service/src/main/java/czy/taotao/config/ConfigCenterApplication.java
+++ config-service/src/main/java/czy/taotao/config/ConfigCenterApplication.java
@@ -10,6 +10,17 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigCenterApplication {
 
 	/**
+	 * 配置中心所有路由为：
+	 *
+	 * /error：错误页面
+	 * /encrypt,methods=POST：对请求体进行加密，返回加密后内容
+	 * /encrypt/{name}/{profiles},methods=POST：对请求体进行加密，目前测试发现路径参数name、profiles不起作用
+	 * /decrypt,methods=POST：对请求体进行解密，返回明文
+	 * /decrypt/{name}/{profiles},methods=POST：对请求体进行揭秘，路径参数无用
+	 * /encrypt/status],methods=GET：加密是否启用
+	 * /key],methods=GET：获取公钥
+	 * /key/{name}/{profiles},methods=GET：也是获取公钥，返回值与上面一样
+	 *
 	 * 配置中心访问配置文件路由占位符：
 	 * name：应用程序名称
 	 * profile：当前激活的profile
@@ -18,7 +29,7 @@ public class ConfigCenterApplication {
 	 * 配置中心访问指定配置文件有以下方法：
 	 * /{name}-{profile}.(yml|json|properties)：这种方式可以直接获取配置文件内容，而没有其他嵌套的字段
 	 * /{label}/name}-{profile}.(yml|json|properties)：这种方式获取结果与上面一样，但是可以加上标签
-	 * /{name}/{profile}：这种方式获取到的配置文件会嵌套在返回jaon结果中，作为一个字段返回
+	 * /{name}/{profile}：这种方式获取到的配置文件会嵌套在返回json结果中，作为一个字段返回
 	 * /{name}/{profile}/{label}：可以获取指定标签内容
 	 *
 	 * 配置中心有关加密的路由有：
