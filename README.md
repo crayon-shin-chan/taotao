5ac60ceadfd37d1f3b1ea070e1031840bd956cd2
chenzhenyuan@skyable.cn <1253665625@qq.com>
2018-11-01 10:29:46 +0800

    迁移配置到配置中心

---
+++ README.md
@@ -0,0 +1,4 @@
+eureka-service端口9999、9998、9997
+config-service端口8888
+zuul-service端口7777
+turbine-service端口6666
--- consumer-service/src/main/java/czy/taotao/consumer/remote/GoodsRemote.java
+++ consumer-service/src/main/java/czy/taotao/consumer/remote/GoodsRemote.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(name="shop-service",fallback = GoodsRemoteFallback.class)
 public interface GoodsRemote {
 
+    /* 使用注解配置，需要调用的服务的接口，参数可以 */
     @GetMapping("/goods/all")
     Goods[] getGoodses();
 
