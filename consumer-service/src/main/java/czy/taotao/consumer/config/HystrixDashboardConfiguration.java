d93d19098098f381d7cd8486e56d8d5086ce7949
chenzhenyuan@skyable.cn <1253665625@qq.com>
2018-11-02 15:26:38 +0800

    修改turbine端口，unavailable-replicas问题不知道怎么，解决了

--- consumer-service/src/main/java/czy/taotao/consumer/config/HystrixDashboardConfiguration.java
+++ consumer-service/src/main/java/czy/taotao/consumer/config/HystrixDashboardConfiguration.java
@@ -14,8 +14,9 @@ public class HystrixDashboardConfiguration {
      * 管理页面通过polling轮训获取最新的熔断器监控情况
      * 注意：一开始未访问服务时，管理页面显示loading字样
      * 直到第一个服务消费请求出现，管理页面才出现监控面板
-     *
      * 默认的配置有一个proxy.stream的流，但是好像不起作用
+     *
+     * 监控页面为/hystrix/monitor，需要设置上面的stream
      */
     @Bean
     public ServletRegistrationBean getServlet() {
