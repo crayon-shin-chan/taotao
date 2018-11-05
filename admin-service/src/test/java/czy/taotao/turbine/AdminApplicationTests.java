1f382ffe1f1d03c174777b5bbefd28fef8a8e06d
czy12345 <1253665625@qq.com>
2018-11-05 23:42:19 +0800

    Admin配置

--- admin-service/src/test/java/czy/taotao/turbine/AdminApplicationTests.java
+++ admin-service/src/test/java/czy/taotao/turbine/AdminApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TurbineApplicationTests {
+public class AdminApplicationTests {
 
 	@Test
 	public void contextLoads() {
