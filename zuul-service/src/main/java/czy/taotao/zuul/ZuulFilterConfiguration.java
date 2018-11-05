73b620010de429e300b1643df750bded7021e7c4
chenzhenyuan@skyable.cn <1253665625@qq.com>
2018-11-05 16:31:00 +0800

    Location重写过滤器

---
+++ zuul-service/src/main/java/czy/taotao/zuul/ZuulFilterConfiguration.java
@@ -0,0 +1,20 @@
+package czy.taotao.zuul;
+
+import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/* zuul内建，但是并未默认使用的过滤器配置 */
+@Configuration
+public class ZuulFilterConfiguration {
+
+    /**
+     * 当web程序使用HTTP状态码3XX重定向时，直接写入的重定向URL为服务的原始路径
+     * LocationRewriteFilter过滤器可以重写Location标头
+     */
+    @Bean
+    public LocationRewriteFilter locationRewriteFilter() {
+        return new LocationRewriteFilter();
+    }
+
+}
